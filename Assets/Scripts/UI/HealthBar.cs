0cf69ae0eb84944e74076565d9082fc117ad5bbd
Mike Han <Mike Han>
2016-01-30 06:20:13 -0800

    Comments for UI script files

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -3,19 +3,23 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class HealthBar : MonoBehaviour {
-	public float curHunger;
-	public float maxHunger;
-	public float recoveryRate;
 
+	// Max hunger point.
+	public float maxHunger;
+	// How fast the hunger bar goes up.
+	public float recoveryRate;
+	// Healthbar image.
 	private Image image;
+
 	void Start()
 	{
 		image = GetComponent<Image> ();
 	}
+
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		// Fills the health bar getting the current position and moves it up using the recoveryRate up to maxHunger point.
 		image.fillAmount = Mathf.MoveTowards(image.fillAmount, maxHunger, Time.deltaTime * recoveryRate);
 	}
 }
--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -5,20 +5,25 @@ using System.Collections;
 public class Score : MonoBehaviour {
 
 	public Text score;
-	private float count;
+	private float scoreCounter;
 
 	void Start ()
 	{
+		// Calls the increaseScore function every 1 second and repeat it every 1 second.
 		InvokeRepeating("increaseScore", 1, 1);
 	}
-
+		
 	void FixedUpdate(){
+		// Every second, update the score text.
 		SetCountText ();
 	}
+
+	// Increase score by 1.
 	void increaseScore(){
-		count++;
+		scoreCounter++;
 	}
 
+	// Sets the score text.
 	void SetCountText ()
 	{
 		score.text = "Score: " + count.ToString ();

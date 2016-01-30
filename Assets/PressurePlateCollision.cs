a38694f2c90c6948184b0affe84bae9cad9eece8
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-01-29 23:14:38 -0800

    Pushing to pull

---
+++ Assets/PressurePlateCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlateCollision : MonoBehaviour {
+    private bool active;
+	// Use this for initialization
+	void Start () {
+        active = false;
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (active)
+                GetComponentInParent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+            else
+                GetComponentInParent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+}

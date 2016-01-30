b0d38cd880babe907f53a37a6f4b1d1cef78f8c9
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-01-30 14:16:50 -0800

    Fixed lunge

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -16,6 +16,9 @@ public class Movement : MonoBehaviour {
 
     //Vertical movement. 1 is up, -1 is down, 0 is no movement.
     private float movey;
+
+    private GameObject target = null;
+    private bool doneLunge = false;
     
     //Start of scripts creation. Used to instantiate variables in our case.
     void Start() {
@@ -25,17 +28,39 @@ public class Movement : MonoBehaviour {
     
     //Called every frame
     void FixedUpdate() {
-        //Get the x and y movement
-        movex = Input.GetAxis("Horizontal");
-        movey = Input.GetAxis("Vertical");
-        //Add velocity to the object based on this velocity.
-        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
+        if (target == null && doneLunge == false)
+        {
+            //Get the x and y movement
+            movex = Input.GetAxis("Horizontal");
+            movey = Input.GetAxis("Vertical");
+            //Add velocity to the object based on this velocity.
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
+        }
+    }
+
+    void Update()
+    {
+        if (target != null && doneLunge == false)
+        {
+            float step = 20 * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
     }
 
     void DashToEnemy(GameObject target)
     {
-        Vector2 direction = (target.transform.position - transform.position).normalized;
-        GetComponent<Rigidbody2D>().AddForce(direction * 10000);
+        this.target = target;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Human" && target != null && Vector2.Distance(transform.position, other.transform.position) < 2)
+        {
+            doneLunge = true;
+            transform.position = Vector3.MoveTowards(transform.position, transform.position, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            Destroy(other.gameObject);
+        }
     }
 }
 

2f80005153a038a9b28b4a629884aae3010213ca
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-01-30 11:55:48 -0800

    DayNight + RayCast Initial

--- Assets/Scripts/Player/VisionConeCollision.cs
+++ Assets/Scripts/Player/VisionConeCollision.cs
@@ -5,8 +5,8 @@ public class VisionConeCollision : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .2f);
+    }
 	
 	// Update is called once per frame
 	void Update () {

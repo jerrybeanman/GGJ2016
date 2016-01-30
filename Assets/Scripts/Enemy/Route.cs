a3a6c0e45c0b65becddc90061667c18fe1f63f88
jerrybeanman <jerry1bean@hotmail.com>
2016-01-30 04:39:07 -0800

    multiple ai working

---
+++ Assets/Scripts/Enemy/Route.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Route : MonoBehaviour {
+
+	public List<Transform> Waypoints;
+	void Awake()
+	{
+		foreach(Transform child in transform)
+			Waypoints.Add(child);
+	}
+}

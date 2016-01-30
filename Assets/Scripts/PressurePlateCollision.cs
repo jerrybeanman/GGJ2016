d8a3bb6390b160de3dd07ce0eba2df2bec9e1e75
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-01-30 01:25:07 -0800

    Modified character stuff

---
+++ Assets/Scripts/PressurePlateCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlateCollision : MonoBehaviour {
+    private bool active;
+    private Vector3 side;
+
+	void Start () {
+        active = false;
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+}

3b3193fc53ee71d125881a82f741a0500ed87de6
jerrybeanman <jerry1bean@hotmail.com>
2016-01-30 15:27:53 -0800

    player rotation working

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -17,25 +17,45 @@ public class Movement : MonoBehaviour {
     //Vertical movement. 1 is up, -1 is down, 0 is no movement.
     private float movey;
     
+	private Animator 	animator;
+	private Rigidbody2D rb2d;
+	private Transform 	VisionTransform;
     //Start of scripts creation. Used to instantiate variables in our case.
-    void Start() {
+    void Start() 
+	{
         movex = 0f;
         movey = 0f;
+		VisionTransform = this.gameObject.transform.GetChild(1);
+		animator = GetComponentInChildren<Animator>();
+		rb2d = GetComponent<Rigidbody2D>();
     }
     
     //Called every frame
     void FixedUpdate() {
         //Get the x and y movement
-        movex = Input.GetAxis("Horizontal");
-        movey = Input.GetAxis("Vertical");
-        //Add velocity to the object based on this velocity.
-        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
-    }
+		movex = Input.GetAxis("Horizontal");
+		movey = Input.GetAxis("Vertical");
+        if(movex == 0 && movey == 0)
+			animator.SetBool("isMoving", false);
+		else
+			animator.SetBool("isMoving", true);
+		//Add velocity to the object based on this velocity.
+		rb2d.MovePosition(rb2d.position + new Vector2(movex, movey) * Speed * Time.fixedDeltaTime);
+		if(movex < 0)
+		{
+			transform.localRotation = Quaternion.Euler(0,0, -90);
 
+
+		}else if(movex > 0)
+		{
+			transform.localRotation = Quaternion.Euler(180,0,90);
+	
+		}
+	}
     void DashToEnemy(GameObject target)
     {
         Vector2 direction = (target.transform.position - transform.position).normalized;
-        GetComponent<Rigidbody2D>().AddForce(direction * 10000);
+		rb2d.AddForce(direction * 10000);
     }
 }
 

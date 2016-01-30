bb1c3201cc733048776bb185affeca1c1b39e509
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-01-29 22:27:48 -0800

    Pushing initial game base

---
+++ Assets/Killable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Killable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "KillMask")
+            Debug.Log("Right tag");
+        //change value to level of kill. If its a swipe, lunge, etc.
+        //other.gameObject.SendMessage("KillPerson", 1);
+    }
+}

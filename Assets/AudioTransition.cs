0befbd2f70dcd0679254033a5e3b1cfe0f7c1159
Mike Han <Mike Han>
2016-01-31 14:51:48 -0800

    Buttons UI changes and extra.

---
+++ Assets/AudioTransition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioTransition : MonoBehaviour {
+
+	GameObject titleMusic = GameObject.Find("TitleMusic");
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+		if (Application.loadedLevel == 6) {
+			Destroy (titleMusic);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/IntroTransition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroTransition : MonoBehaviour {
+
+	GameObject titleMusic = GameObject.Find ("TitleMusic");
+	private float time;
+	private int scene;
+	// Use this for initialization
+	void Start () {
+		time = 0.0f;
+		InvokeRepeating("Timer", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (time >= 4.0f) {
+			scene = Application.loadedLevel;
+			scene++;
+			Application.LoadLevel(scene);
+		}
+	}
+
+	void Timer(){
+		time++;
+	}
+	public void StartGame(){
+		Destroy (titleMusic);
+		Debug.Log ("test");
+		Application.LoadLevel (6);
+	}
+}

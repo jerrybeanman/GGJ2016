50a8e5e0a69c1b2fbf9076a6dd0e01874c8d1cb6
Jaegar Sarauer <jaegars@hotmail.com>
2016-01-31 14:50:45 -0800

    Faster eating.

--- Assets/Scripts/Player/HungerSystem.cs
+++ Assets/Scripts/Player/HungerSystem.cs
@@ -4,11 +4,16 @@ using System.Collections;
 
 public class HungerSystem : MonoBehaviour {
     Collider2D other;
+
+    void Update() {
+    }
+
     // On collision with food, increase our time til we die by 10 seconds.
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Food")
         {
+            other.gameObject.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f ,0f);
             other.GetComponent<AudioSource>().UnPause();
 			HealthBar.Instance.image.fillAmount -= (float)(other.gameObject.GetComponent<Food>().type+1) * 0.1f;
             this.other = other;
